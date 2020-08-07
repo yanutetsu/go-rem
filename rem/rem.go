1628b657ac71db09e5ef53bf61b35fa4093aa13b
yanutetsu <satoshi.yanuma@topgate.co.jp>
2020-08-07 19:01:23 +0900

    show rem's face

--- rem/rem.go
+++ rem/rem.go
@@ -1,25 +1,10 @@
 package rem
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-var remsays = [...]string{
-	"姉様姉様。どうやら少し混乱されているみたいですお客様",
-	"鬼がかってますね。",
-	"嘘をついていることぐらいレムにもわかります。その嘘の理由が話せないでいることも、わかります。だから信じさせようだとか、嘘で丸め込もうだとか、そんな風に自分を追い詰めたりする必要、どこにもないんですよ。だってレムは　丸ごと信じていますから。",
-	"いえ、そんなことは・・・レムも少し、かなり少し、とても少し気になるのは事実ですから",
-	"穀潰しの発言ですよ。聞きました姉様？",
-}
-
-// Print prints a message with rem's random message.
+// Print prints a message by rem.
 // It works like fmt.Print.
 // You can use it only by changing from `fmt` to `rem`.
 func Print(a ...interface{}) {
-	msg := fmt.Sprint(a...)
-
-	rand.Seed(time.Now().UnixNano())
-	fmt.Printf("レム＜%s %s", remsays[rand.Intn(5)], msg)
+	fmt.Printf("(///ᴗㆁ✿) ＜ %s", fmt.Sprint(a...))
 }
